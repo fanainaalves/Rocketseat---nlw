70d6f4ff6227ebb08177be99d8624983059cba29
Fanaina Alves <fanaiina.allves@gmail.com>
2020-06-07 22:48:15 -0300

    escape

---
+++ src/server.js
@@ -0,0 +1,90 @@
+const express = require("express")
+const server = express()
+
+const db = require("./database/db.js")
+
+server.use(express.static("public"))
+server.use(express.urlencoded({ extended: true}))
+
+const nunjucks = require("nunjucks")
+
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+} )
+server.get("/", (req, res) => {
+    res.render("index.html", {title: "um titulo"})
+})
+server.get("/create-point", (req, res) => {
+console.log(req.query)
+
+
+    return res.render("create-point.html")
+})
+server.post("/savepoint", (req, res)=>{
+    //console.log(req.body)
+    // console.log(req.query)
+
+        const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);`
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+
+    function afterInsertData(err){
+
+        if (err){
+            console.log(err)   
+            return res.send("Erro no cadastro")      
+        }
+
+        console.log("Cadastrado com sucesso!")
+        console.log(this)
+
+        return res.send("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+
+    
+})
+
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+    if (search == ""){
+        return res.render("search-results.html",{total: 0})
+    }
+
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+        if (err){
+            return console.log(err)   
+        }
+       // console.log(rows)
+        const total = rows.length
+        return res.render("search-results.html", { places: rows, total: total})
+    })
+            
+    
+})
+
+
+server.listen(3000)
